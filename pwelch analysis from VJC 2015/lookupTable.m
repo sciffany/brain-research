505d7963ecd57419f81d77d327ade0ab629fbb39
sciffany <sciffany@gmail.com>
2019-04-05 15:43:23 +0800

    Remove guide files

--- pwelch analysis from VJC 2015/lookupTable.m
+++
@@ -1,6 +0,0 @@
-function index= lookupTable( CateList,LabelList,selCate)
-
-index=find(CateList==selCate);
-
-end
-
--- pwelch analysis from VJC 2015/pxxAnalysis.m
+++
@@ -1,6 +0,0 @@
-%get the supermatrix
-for isub=1:1:5;
-    for icate=171:1:176;
-        supermatrix(:,:,:,icate,isub)=GetPxx(icate,isub);
-    end
-end
